cd351e917ee6ac1908944fa10bc59eeac0e7136f
rjramos70 <rjramos70@gmail.com>
2018-04-12 23:29:56 -0300

    Atualizacao da classe CadastroPjDto

--- src/main/java/br/com/meudominio/pontointeligente/api/dtos/CadastroPjDto.java
+++ src/main/java/br/com/meudominio/pontointeligente/api/dtos/CadastroPjDto.java
@@ -1,5 +1,11 @@
 package br.com.meudominio.pontointeligente.api.dtos;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.hibernate.validator.constraints.br.CPF;
+
 public class CadastroPjDto {
 
 	private Long id;
@@ -10,7 +16,88 @@ public class CadastroPjDto {
 	private String razaoSocial;
 	private String cnpj;
 	
-	// parei em 02:42 / 10:23
+	public CadastroPjDto() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	// Abaixo as anotacoes das validacoes do campo nome
+	// Campo nome e obrigatorio
+	@NotEmpty(message = "Nome nao pode ser vazio.")
+	// Quantidade minima e maxima de caracteres
+	@Length(min = 3, max = 200, message = "Nome deve conter entre 3 e 200 caracteres.")
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@NotEmpty(message = "Email nao pode ser vazio.")
+	@Length(min = 5, max = 200, message = "Email deve conter entre 5 e 200 caracteres.")
+	@Email(message = "Email invalido.")
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@NotEmpty(message = "Senha nao pode ser vazia.")
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@NotEmpty(message = "CPF nao pode ser vazio.")
+	@CPF(message = "CPF invalido.")
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@NotEmpty(message = "Razao Social nao pode ser vazio.")
+	@Length(min = 5, max = 200, message = "Razao Social deve conter entre 5 e 200 caracteres.")
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	@NotEmpty(message = "CNPJ nao pode ser vazio.")
+	@CNPJ(message = "CNPJ invalido.")
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	@Override
+	public String toString() {
+		return "CadastroPjDto [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + ", cpf=" + cpf
+				+ ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + "]";
+	}
+	
+	
 	
 	
 }
