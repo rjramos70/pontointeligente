9668657ad9d73886edf0a92a0ef302d8d67322b4
rjramos70 <rjramos70@gmail.com>
2018-04-17 00:38:03 -0300

    Verificacao e atualizacao da classe LancamentoRepository.java que esta gerando erro no TravisCI

--- src/main/java/br/com/meudominio/pontointeligente/api/repositories/LancamentoRepository.java
+++ src/main/java/br/com/meudominio/pontointeligente/api/repositories/LancamentoRepository.java
@@ -13,14 +13,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import br.com.meudominio.pontointeligente.api.entities.Lancamento;
 
-@Transactional( readOnly = true)	// Essa interface inteira e so de leitura ao banco, por ser só uma transação de consulta, o mesmo nao trava o banco.
+@Transactional(readOnly = true)
 @NamedQueries({
-	@NamedQuery( name = "LancamentoRepository.findByFuncionarioId",	// Esse sera o nome da query quando fomos chamar no Spring.
-				query = "SELECT lanc FROM Lancamento lanc WHERE lanc.funcionario.id = :funcionarioId"	// Query montada em JPQl, SQL so JPA. 
-			   )})
-public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
+		@NamedQuery(name = "LancamentoRepository.findByFuncionarioId", 
+				query = "SELECT lanc FROM Lancamento lanc WHERE lanc.funcionario.id = :funcionarioId") })
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
-	List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);	// Metdo retorna todos os funcionarios.
-	
-	Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pagable);	// Metdo retorna so resultados paginados, pode pedir do primeiro ao decimo elemento como exemplo.
+	List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
+
+	Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
 }
