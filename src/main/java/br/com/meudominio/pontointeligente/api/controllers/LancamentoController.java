dc68e581dfeac17ee4f6d23ed9698f33e427d8bf
rjramos70 <rjramos70@gmail.com>
2018-05-10 00:41:33 -0300

    Trabalhando na classe LancamentoController.java

--- src/main/java/br/com/meudominio/pontointeligente/api/controllers/LancamentoController.java
+++ src/main/java/br/com/meudominio/pontointeligente/api/controllers/LancamentoController.java
@@ -1,8 +1,12 @@
 package br.com.meudominio.pontointeligente.api.controllers;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.apache.commons.lang3.EnumUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +15,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.meudominio.pontointeligente.api.dtos.LancamentoDto;
+import br.com.meudominio.pontointeligente.api.entities.Funcionario;
 import br.com.meudominio.pontointeligente.api.entities.Lancamento;
+import br.com.meudominio.pontointeligente.api.enums.TipoEnum;
 import br.com.meudominio.pontointeligente.api.response.Response;
 import br.com.meudominio.pontointeligente.api.services.FuncionarioService;
 import br.com.meudominio.pontointeligente.api.services.LancamentoService;
@@ -85,14 +95,99 @@ public class LancamentoController {
 		
 	}
 
+	/**
+	 * Adiciona um novo lancamento
+	 * 
+	 * @param id
+	 * @return ResponseEntity<Response<LancamentoDto>>
+	 */
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Response<LancamentoDto>> listarPorId(@PathVariable("id") Long id){
+		log.info("Buscando lancamento por ID: {}", id);
+		
+		Response<LancamentoDto> response = new Response<LancamentoDto>();
+		Optional<Lancamento> lancamento = this.lancamentoService.buscarPorId(id);
+		
+		// Se nao for encontrado o lancamento
+		if (!lancamento.isPresent()) {
+			log.info("Lancamento nao encontrado para o ID: {}", id);
+			response.getErrors().add("Lancamento nao encontrado para o ID: {}" + id);
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		// Se lancamento existir na base de dados
+		response.setData(this.converteLancamentoDto(lancamento.get()));
+		return ResponseEntity.ok(response);
+	}
 	
-	// FALTA COMPLEMENTAR O RESTPO DO CODIGO !!!
+	
+	@PostMapping
+	public ResponseEntity<Response<LancamentoDto>> adicionar(@Valid @RequestBody LancamentoDto lancamentoDto,
+			BindingResult result) throws ParseException{
+		
+		log.info("Adicionando lancamento: {}", lancamentoDto.toString());
+		
+		Response<LancamentoDto> response = new Response<LancamentoDto>();
+		
+		validarFuncionario(lancamentoDto, result);
+		
+		Lancamento lancamento = this.converteDtoParaLancamento(lancamentoDto, result);
+		
+		// Se houver algum erro
+		if (result.hasErrors()) {
+			log.error("Erro validando lancamento: {}", result.getAllErrors());
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		lancamento = this.lancamentoService.persistir(lancamento);
+		return ResponseEntity.ok(response);
+	}
+	
+	
+	
+	
+	// Recomecar no metodo atualizar...
 	
 	
 	
 	
 	
 	
+	
+	
+
+	/**
+	 * Valida um funcionario, verificando se ele e existente e valido no sistema
+	 * 
+	 * @param lancamentoDto
+	 * @param result
+	 */
+	private void validarFuncionario(LancamentoDto lancamentoDto, BindingResult result) {
+		
+		if (lancamentoDto.getFuncionarioId() == null) {
+			result.addError(new ObjectError("funcionario", "Funcionario nao informado."));
+			return;
+		}
+		
+		log.info("Validando funcionario de ID {}:", lancamentoDto.getFuncionarioId());
+		
+		Optional<Funcionario> funcionario = this.funcionarioService.buscarPorId(lancamentoDto.getFuncionarioId());
+		
+		// Se nao for identificado funcionario com o respectivo ID
+		if (!funcionario.isPresent()) {
+			result.addError(new ObjectError("funcionario", "Funcionario naop enontrado. ID inexistente."));
+		}
+		
+		
+	}
+
+	/**
+	 * Converte uma entidade lancamento para seu respectivo DTO.
+	 * 
+	 * @param lancamento
+	 * @return LancamentoDto
+	 */
 	private LancamentoDto converteLancamentoDto(Lancamento lancamento) {
 		LancamentoDto lancamentoDto = new LancamentoDto();
 		
@@ -106,5 +201,39 @@ public class LancamentoController {
 		return lancamentoDto;
 	}
 	
-	
+	/**
+	 * Converte um LancamentoDto para uma entidade Lancamento.
+	 * 
+	 * @param  lancamentoDto
+	 * @param  result
+	 * @return Lancamento
+	 * @throws ParseException
+	 */
+	private Lancamento converteDtoParaLancamento(LancamentoDto lancamentoDto, BindingResult result) throws ParseException {
+		Lancamento lancamento = new Lancamento();
+
+		if (lancamentoDto.getId().isPresent()) {
+			Optional<Lancamento> lanc = this.lancamentoService.buscarPorId(lancamentoDto.getId().get());
+			if (lanc.isPresent()) {
+				lancamento = lanc.get();
+			} else {
+				result.addError(new ObjectError("lancamento", "Lançamento não encontrado."));
+			}
+		} else {
+			lancamento.setFuncionario(new Funcionario());
+			lancamento.getFuncionario().setId(lancamentoDto.getFuncionarioId());
+		}
+
+		lancamento.setDescricao(lancamentoDto.getDescricao());
+		lancamento.setLocalizacao(lancamentoDto.getLocalizacao());
+		lancamento.setData(this.dateFormat.parse(lancamentoDto.getData()));
+
+		if (EnumUtils.isValidEnum(TipoEnum.class, lancamentoDto.getTipo())) {
+			lancamento.setTipo(TipoEnum.valueOf(lancamentoDto.getTipo()));
+		} else {
+			result.addError(new ObjectError("tipo", "Tipo inválido."));
+		}
+
+		return lancamento;
+	}
 }
