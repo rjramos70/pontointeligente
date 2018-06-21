fb2f8f0beb502fed563360db6351c527f1676f55
rjramos70 <rjramos70@gmail.com>
2018-06-21 01:48:15 -0300

    Atualizacao da classe JwtUser, pois os metodos getPassword e getAuthrities estavao retornando null inves das variaceis

--- src/main/java/br/com/meudominio/pontointeligente/api/security/JwtUser.java
+++ src/main/java/br/com/meudominio/pontointeligente/api/security/JwtUser.java
@@ -55,15 +55,13 @@ public class JwtUser implements UserDetails {
 	
 	@Override
 	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.password;
 	}
 	
 	
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.authorities;
 	}
 
 
